cc2004d761280d4e100d5966e9820719e7a207e3
Meraj Kashi <mkashi@eikplatform.io>
2021-04-24 20:13:15 +0200

    Copy all dirs

--- deployer.sh
+++ deployer.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-docker build -t php56 ./bin/php56/Dockerfile
-docker build -t mysql ./bin/mysql8/Dockerfile
-docker build -t nodejs ./bin/nodejs/Dockerfile
+docker build -t php56 ./bin/php56/
+docker build -t mysql ./bin/mysql8/
+docker build -t nodejs ./bin/nodejs/
+
+sudo cp -r ./config/ /mnt/
+sudo cp -r ./www/ /mnt/
+sudo cp -r ./logs /mnt/
+sudo cp -r ./data/ /mnt/
 
 docker stack deploy --compose-file docker-compose.yml projectx
 
