d14d6fcc1958b40526fe43db2c3e14c108a66919
Meraj Kashi <mkashi@eikplatform.io>
2021-04-26 21:03:22 +0200

    Modify compose files

--- deployer.sh
+++ deployer.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
-docker build -t php56 ./bin/php56/
-docker build -t mysql ./bin/mysql8/
-docker build -t nodejs ./bin/nodejs/
-
 sudo cp -r ./config/ /mnt/
 sudo cp -r ./www/ /mnt/
 sudo cp -r ./logs /mnt/
 sudo cp -r ./data/ /mnt/
+sudo cp -r ./bacula/etc/
+sudo mkdir -p /mnt/esdata || true
+sudo mkdir -p /mnt/logstash-pipeline ||true
 
 docker stack deploy --compose-file docker-compose.yml projectx
+docker stack deploy --compose-file ./elastic/docker-compose.yml logging
+docker stack deploy --compose-file ./bacula/docker-compose.yml backup
+
 
