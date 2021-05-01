00623b4f20cc205efd63600d22ee1cfd847353c4
nurrebass1 <16683497+nurrebass1@users.noreply.github.com>
2021-05-01 11:02:24 +0200

    added new dirs in deployer

--- deployer.sh
+++ deployer.sh
@@ -5,7 +5,9 @@ sudo cp -r ./logs /mnt/
 sudo cp -r ./data/ /mnt/
 sudo cp -r ./bacula/etc/ /mnt/
 sudo mkdir -p /mnt/esdata || true
-sudo mkdir -p /mnt/logstash-pipeline ||true
+sudo mkdir -p /mnt/logstash-pipeline || true
+sudo mkdir -p /usr/share/logstash/pipeline/ || true
+sudo mkdir -p /usr/share/filebeat/ || true
 
 docker stack deploy --compose-file docker-compose.yml projectx
 docker stack deploy --compose-file ./elastic/docker-compose.yml logging
